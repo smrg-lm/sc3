c3052c27bfc47426384ce5fa083b3b32ae183644
Lucas Samaruga <samarugalucas@gmail.com>
2019-01-12 18:35:37 -0300

    Agregados a Client los métodos msg_size, bundle_size, clump_bundle y completado sync. Pero tengo que revisar los valores y si los bundles pueden ser recursivos en sclang/scsynth, me confunden las nomenclaturas en sclang.

--- client.py
+++ client.py
@@ -100,6 +100,7 @@ class Client(object):
     def send_bundle(self, target, time, *args): # // sclang warning: this primitive will fail to send if the bundle size is too large # // but it will not throw an error.  this needs to be fixed
         """args son listas de valores para crear varios mensajes"""
         messages = [_lo.Message(*x) for x in args] # BUG: falta la conversión de tipos con tupla
+                                                   # BUG: ver si los bundles en sc pueden ser recursivos!!!
         time = time or 0 # BUG: qué pasaba con valores negativos?
         bundle = _lo.Bundle(float(time), *messages)
         self._osc_server_thread.send(target, bundle)
@@ -107,17 +108,33 @@ class Client(object):
     def send_status_msg(self):
         self.send_msg('/status')
 
-    # def send_clumped_bundles(self, time, *args): # TODO: importante, lo usa para enviar paquetes muy grandes como stream, ver que hace liblo.
-
-    def sync(self, condition=None, bundles=None, latency=None): # TODO: importante, es la fuente de s.sync
+    def sync(self, condition=None, bundle=None, latency=0): # BUG: dice array of bundles, los métodos bundle_size y send_bundle solo pueden enviar uno. No me cierra/me confunde en sclang porque usa send bundle agregándole latencia.
         condition = condition or _threading.Condition()
-        if bundles is None:
+        if bundle is None:
             id = self.make_sync_responder(condition)
             self.send_bundle(('127.0.0.1', 57120), latency, ['/sync', id]) # TEST BUG: falta default target que creo que sería el servidor por defecto, está puesto sclang
             with condition:
                 condition.wait() # BUG: poner timeout y lanzar una excepción?
         else:
-            raise NotImplementedError('Implementar sync con bundles') # TODO
+            # BUG: esto no está bien testeado, y acarreo el problema del tamaño exacto de los mensajes.
+            sync_size = self.msg_size(['/sync', ut.UniqueID.next()])
+            max_size = 65500 - sync_size # BUG: 65500 es un límite práctico que puede estar mal si las cuentas de abajo están mal.
+            if self.bundle_size(bundle) > max_size:
+                clumped_bundles = self.clump_bundle(bundle, max_size)
+                for item in clumped_bundles:
+                    id = self.make_sync_responder(condition)
+                    item.append(['/sync', id])
+                    self.send_bundle(('127.0.0.1', 57120), latency, *item) # TEST BUG: falta default target que creo que sería el servidor por defecto, está puesto sclang
+                    latency += 1e-9 # nanosecond, TODO: esto lo hace así no sé por qué.
+                    with condition:
+                        condition.wait() # BUG: poner timeout y lanzar una excepción?
+            else:
+                id = self.make_sync_responder(condition)
+                bundle = bundle[:]
+                bundle.append(['/sync', id])
+                self.send_bundle(('127.0.0.1', 57120), latency, *bundle) # TEST BUG: falta default target que creo que sería el servidor por defecto, está puesto sclang
+                with condition:
+                    condition.wait() # BUG: poner timeout y lanzar una excepción?
 
     def make_sync_responder(self, condition): # TODO: funciona en realación al método de arriba.
         id = ut.UniqueID.next()
@@ -129,3 +146,61 @@ class Client(object):
                     condition.notify()
         self._osc_server_thread.add_method('/synced', 'i', responder)
         return id
+
+    # def send_clumped_bundles(self, time, *args): # TODO: importante, lo usa para enviar paquetes muy grandes como stream, liblo tira error y no envía
+    def clump_bundle(self, msg_list, new_bundle_size): # msg_list siempre es un solo bundle [['/path', arg1, arg2, ..., argN], ['/path', arg1, arg2, ..., argN], ...]
+        ret = [[]]
+        clump_count = 0
+        acc_size = 0
+        aux = 0 # solo inicializo porque se usa dentro del loop
+        for item in msg_list:
+            aux = self.msg_size(item)
+            if acc_size + aux > new_bundle_size: # BUG: 65500 es un límite práctico que puede estar mal si las cuentas de abajo están mal.
+                acc_size = aux
+                clump_count += 1
+                ret.append([])
+                ret[clump_count].append(item)
+            else:
+                acc_size += aux
+                ret[clump_count].append(item)
+        if len(ret[0]) == 0: ret.pop(0)
+        return ret
+
+    def msg_size(self, arg_list): # arg_list siempre es ['/path', arg1, arg2, ..., argN]
+        size = 0 # bytes
+        typetags = -1 # path no cuenta
+        aux = 0 # solo inicializo porque se usa dentro del loop
+        for item in arg_list:
+            t = type(item)
+            if t is str:
+                aux = len(item)
+                mod4 = aux & 3 # aux % 4
+                size += aux
+                if mod4:
+                    size += 4 - mod4 # alineamiento
+                else:
+                    size += 4 # null y alineamiento
+            elif t is bytes:
+                aux = 4 # size count
+                aux += len(item)
+                mod4 = aux & 3 # aux % 4
+                size += aux
+                if mod4:
+                    size += 4 - mod4 # alineamiento
+            elif t is int or t is float or t is tuple:
+                size += 4 # BUG: 8 si se usan doubles
+            else:
+                raise TypeError('invalid type ({}) for OSC message'.format(t)) # BUG: pueden haber tipos de datos que son válidos porque liblo traduce después?
+            typetags += 1
+        size += 1 # 1 byte para la ',' del type tag
+        size += typetags # type tag por cada argumento osc
+        mod4 = (1 + typetags) & 3 # (1 + typetags) % 4
+        if mod4:
+            size += 4 - mod4 # alienamiento
+        return size # bytes # + 12 # BUG: falta algo, acá se pueden sumar 12 por: 8 bytes udp (BUG: depende de proto) header, 20 bits (redondeado a 4 bytes con alineamiento aunque no se si corresponde) ip header, igual me faltan alrededor de 20 bytes entre msg y bundle, salvo que sea algo dinámico y me falen más. En sclang hay una nota que justo son 20 bytes: // 65515 = 65535 - 16 - 4 (sync msg size)
+
+    def bundle_size(self, args): # args siempre es [['/path', arg1, arg2, ..., argN], ['/path', arg1, arg2, ..., argN], ...]
+        size = 0
+        for item in args:
+            size += self.msg_size(item)
+        return size + 40 # bytes # BUG: 8 bytes para '#bundle', 8 para time tag, 4 para el tamaño del atado, pero la cuenta en msg está mal, falta(n) algo(s). Y creo que los mensajes pueden tener distinto formato, no me quedó claro.

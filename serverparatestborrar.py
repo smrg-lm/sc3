402419ace9b197e919c06b8a507fef6131bbeebd
Lucas Samaruga <samarugalucas@gmail.com>
2019-01-23 14:02:26 -0300

    Copiado del servidor provisorio para pruebas.

--- serverparatestborrar.py
+++ serverparatestborrar.py
@@ -1,4 +1,11 @@
-"""Server.sc"""
+"""
+Se encarga de:
+    * iniciar y terminar scsynth/supernova
+    * iniciar shm
+    * monitorear el estatus?
+    * quién envía los msj osc?
+    * ...
+"""
 
 import subprocess as _subprocess
 import threading as _threading
@@ -14,97 +21,16 @@ import supercollie.client as cl
 
 class ServerOptions(object):
     def __init__(self):
-        self.num_audio_bus_channels = 1024 # solo getter # TODO: tiene setter especial
-        self.num_control_bus_channels = 16384 # getter/setter
-        self.num_input_bus_channels = 2 # solo getter # TODO: tiene setter especial
-        self.num_output_bus_channels = 2 # solo getter # TODO: tiene setter especial
-        self.num_buffers = 1026; # getter setter
+        self.program = 'scsynth' # test
+        self.cmd_options = [
+            '-u', '57110', '-a', '1024',
+            '-i', '2', '-o', '2', '-b', '1026',
+            '-R', '0', '-C', '0', '-l', '1'] # test
 
-        self.max_nodes = 1024 # Todos los métodos siguientes tiene getter y setter salvo indicación contraria
-        self.max_synth_defs = 1024
-        self.protocol = 'UDP'
-        self.block_size = 64
-        self.hardware_buffer_size = None
+    def cmd(self):
+        return [self.program] + self.cmd_options
 
-        self.mem_size = 8192
-        self.num_rgens = 64
-        self.num_wire_bufs = 64
-
-        self.sample_rate = None
-        self.load_defs = True
-
-        self.input_streams_enabled = # None/False # no inicializada por defecto
-        self.output_streams_enabled = # None/False # no inicializada por defecto
-
-        self.in_device = None
-        self.out_device = None
-
-        self.verbosity = 0
-        self.zeroconf = False # // Whether server publishes port to Bonjour, etc.
-
-        self.restricted_path = None
-        self.ugen_plugins_path = None
-
-        self.initial_node_id = 1000
-        self.remote_control_volume = False
-
-        self.memory_locking = False
-        self.threads = None # // for supernova
-        self.use_system_clock = False # // for supernova
-
-        self.num_private_audio_bus_channels = 1020 # solo getter # TODO: tiene setter especial
-
-        self.reserved_num_audio_bus_channels = 0
-        self.reserved_num_control_bus_channels = 0
-        self.reserved_num_buffers = 0
-        self.pings_before_considered_dead = 5
-
-        self.max_logins = 1
-
-        self.rec_header_format = 'aiff'
-        self.rec_sample_format = 'float'
-        self.rec_channels = 2
-        self.rec_buf_size = None
-
-        # @property
-        self._device = None # BUG: ver el valor por defecto.
-
-    @property
-    def device(self): pass
-    @device.setter
-    def device(self, value): pass
-    # TODO: esta propiedad también tiene un método de clase lo mismo que inDevices and outDevices
-
-    def as_options_string(self, port=57110): pass
-
-    def first_private_bus(self): pass
-    def boot_in_process(self): pass
-    # numPrivateAudioBusChannels_ { |numChannels = 112|
-    # numAudioBusChannels_ { |numChannels=1024|
-    # numInputBusChannels_ { |numChannels=8|
-    # numOutputBusChannels_ { |numChannels=8|
-
-    def recalc_channels(self): pass
-
-    # *prListDevices { # TODO: llama a primitiva
-    # *devices { # llama a *prListDevices
-    # *inDevices { # llama a *prListDevices
-    # *outDevices { # llama a *prListDevices
-
-
-class ServerShmInterface():
-    def __init__(self, port):
-        self.ptr = None # BUG variable de bajo nivel, depende de la implementación.
-        self.finalizer = None # BUG variable de bajo nivel, depende de la implementación.
-        self.connect(port) # BUG: llama a una primitiva y debe guardar port a bajo nivel
-
-    # copy # // never ever copy! will cause duplicate calls to the finalizer!
-    def connect(self, port): pass # primitiva
-    def disconnect(self): pass # primitiva
-    def get_control_bus_value(self): pass # primitiva
-    def get_control_bus_values(self): pass # primitiva
-    def set_control_bus_value(self, value): pass # primitiva # BUG: desconozco los parámetros.
-    def set_control_bus_values(self, *values): pass # primitiva # BUG: desconozco los parámetros.
+    # TODO: todo...
 
 
 @ut.initclass

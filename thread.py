51b176ac8f9cc8ce3d6186c8e5601c0696d78057
Lucas Samaruga <samarugalucas@gmail.com>
2019-02-05 06:34:50 -0300

    Comentarios para recordar y borrar luego, es complicado.

--- thread.py
+++ thread.py
@@ -9,6 +9,15 @@
 # TODO: ver https://stackoverflow.com/questions/750702/retrieving-printing-execution-context
 # ^ CREO QUE NINGUNO DE ESOS RECURSOS ME SIRVE ^
 
+# TODO: Process y Main (juntas o por separado, o solo Process) serían
+# TODO: el entorno de ejecusión de SuperCollider dentro de Python.
+# TODO: Probablemente sea lo mejor usar Process/Main para el manejo
+# TODO: de los Threads como controlador global. No sé si no lo mismo
+# TODO: con las cosas de synthdef en _global.py. Process es un nombre
+# TODO: acertado para esto, en vez de Client, que sería oscclient o
+# TODO: parte de la implementación de Process. SCLang hace muchas cosas
+# TODO: distintas específicas del lenguaje que acá no son necesarias.
+
 import enum
 import inspect
 
@@ -27,7 +36,11 @@ class TimeThread(): #(Stream): # BUG: hereda de Stream por Routine y no la usa, 
         if not inspect.isfunction(func):
             raise TypeError('Thread function arg is not a function')
 
-        self.beats = current_TimeThread.beats # BUG: no se de dónde sacar parent, si pasarlo como argumento o como una propiedad current_thread de la clase
+        # BUG: ver test_clock_thread.scd, estos valores no tienen efecto porque
+        # se sobreescribe el reloj por TempoClock.default en Stream:play
+        # y Routine:run vuelve a escribir SystemClock (cuando ya lo hizo en
+        # PyrPrimitive). La única manera de usar el reloj heredado es llamando a next.
+        self.beats = current_TimeThread.beats # BUG: no se de dónde sacar parent, PyrPrimitive usa &g->thread->beats, BUG: PERO NUNCA SETEA ESA PROPIEDAD! si pasarlo como argumento o como una propiedad current_thread de la clase
         self.seconds = current_TimeThread.seconds # BUG: creo que tienen setters porque son dependientes...
 
         self.func = func
@@ -41,6 +54,10 @@ class TimeThread(): #(Stream): # BUG: hereda de Stream por Routine y no la usa, 
         else:
             self.clock = current_TimeThread.clock # hace slotCopy y lo registra en GC, puede que solo esté marcando una referencia más, más arriba a GCWriteNew con el array de la pila que es nuevo
 
+        # NOTA: No guarda la propiedad <parent cuando crea el thread, es
+        # &g->thread que la usa para setear beats y seconds pero no la guarda,
+        # la setea luego en algún lugar, con el cambio de contexto, supongo,
+        # y tiene valor solo mientras la rutina está en ejecusión. Ver test_clock_thread.scd
         self.environment = current_Environment # BUG: acá llama al slot currentEnvironment de Object y lo setea al del hilo
         # BUG: acá setea nowExecutingPath de la instancia de Process (Main) si es que no la estamos creando con este hilo.
         # if(g->process) { // check we're not just starting up

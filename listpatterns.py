050c134a9dd3b1f4ab9f868d6772b6263645802f
Lucas Samaruga <samarugalucas@gmail.com>
2019-03-18 00:40:15 -0300

    Revestimiento (aka boilerplate) con comentarios de ListPatterns.sc y Ppar.sc.

--- listpatterns.py
+++ listpatterns.py
@@ -29,9 +29,9 @@ class Pseq(ListPattern):
         self.offset = offset
 
     def __embed__(self, inval=None):
-        offset = self.offset # BUG: en realidad llama a value todo este if/else no específicamente next.
-                             # BUG: ************** puede ser que llame a value no porque sea un stream sino una función que
-                             # BUG: ************** modifican el comportamiento de repeats y offset según los datos el evento.
+        offset = self.offset # BUG: *** en el original llama a value (no a next).
+                             # BUG: *** puede ser que llame a value no porque sea un stream sino una función que
+                             # BUG: *** modifican el comportamiento de repeats y offset según los datos el evento.
         # if (inval.eventAt('reverse') == true, { # BUG: TODO: No se si es bueno que el evento defina el comportamiento dle pattern.
         repeats = self.repeats
         lst = collections.deque(self.lst)
@@ -43,10 +43,97 @@ class Pseq(ListPattern):
     # storeArgs # TODO
 
 
+# Es una variante de Pseq que cuenta por item en lugar de por lista
+class Pser(Pseq):
+    pass
 
 
+class Pshuf(ListPattern):
+    pass
 
 
+class Prand(ListPattern):
+    pass
+
+class Pxrand(ListPattern):
+    pass
 
 
-# TODO: sigue
+class Pwrand(ListPattern):
+    pass
+
+
+# # TODO: estos dos son un tanto específicos.
+# # // Finite State Machine
+# class Pfsm(ListPattern):
+#     pass
+# # // Deterministic Finite State Machine
+# class Pdfsm(ListPattern):
+#     pass
+
+
+# TODO: selecciona elementos por índice en la lista.
+class Pswitch(ptt.Pattern):
+    pass
+
+
+# TODO: idem pero no embebe el item si es un stream sino que alterna por elemento
+class Pswitch1(Pswitch):
+    pass
+
+
+# TODO: es un tipo de paralelización de streams.
+class Ptuple(ListPattern):
+    pass
+
+
+# TODO: entrelaza elementos de sub listas, es secuencial.
+class Place(Pseq):
+    pass
+
+
+# TODO: este es un caso de funcionalidad repetida por tipo de dato
+# de entrada, este tipo de cosas estaría bueno unificar, como hace
+# en general en el resto de la librería. Pero es un detalle por ahora.
+# // similar to Place, but the list is an array of Patterns or Streams
+class Ppatlace(Pseq):
+    pass
+
+
+# TODO: este es una operación específica, una manera de recorrer
+# la lista. Ver si no se puede hacer programáticamente de otra manera,
+# aunque es el mismo principio que con rand, xrand, walk, etc.
+class Pslide(ListPattern):
+    # // 'repeats' is the number of segments.
+    # // 'len' is the length of each segment.
+    # // 'step' is how far to step the start of each segment from previous.
+    # // 'start' is what index to start at.
+    # // indexing wraps around if goes past beginning or end.
+    # // step can be negative.
+    pass
+
+
+class Pwalk(ListPattern):
+    pass
+
+
+### Ppar.sc ###
+
+
+# TODO: es un ListPattern, pero es exclusivamente para event streams,
+# tampoco es que haya otra forma, Ptuple tal vez, se necesita delta.
+# En este sentido actúa como un conjunto de elementos específico (listas).
+class Ppar(ListPattern):
+    pass
+
+
+class Ptpar(Ppar):
+    pass
+
+
+class Pgpar(Ppar):
+    pass
+
+
+class Pgtpar(Pgpar):
+    pass

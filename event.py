63224a8230b27c51abac8a83cff5d50c26ed7731
Lucas Samaruga <samarugalucas@gmail.com>
2019-03-31 03:19:16 -0300

    Event aún es místico, tal vez devería pasar a otra cosa.

--- event.py
+++ event.py
@@ -1,5 +1,7 @@
 """Event.sc"""
 
+import types
+
 
 class Event(dict):
     default_parent_event = {}
@@ -14,20 +16,42 @@ class Event(dict):
 
     # NOTE: set/get/del es simplemente para implementar y las llamadas a super
     # __setattr__ son solo por know = always True, no se si realmente hace
-    # alguna diferencia realmente...
+    # alguna diferencia...
     def __setattr__(self, name, value):
         if hasattr(type(self), name):
             raise AttributeError(f"attribute '{name}' can't be used as key")
         self[name] = value
 
-    # NOTE: ver por qué, y si es parte de la especificación, este método
-    # igula llama siempre los atributos de la superclase.
+    # NOTE: https://docs.python.org/3/howto/descriptor.html#invoking-descriptors
+    # NOTE: "Called when the default attribute access fails with an AttributeError"
+    # NOTE: https://docs.python.org/3/reference/datamodel.html#object.__getattribute__
+    # NOTE: https://docs.python.org/3/reference/datamodel.html#special-lookup
+    # NOTE: https://docs.python.org/3/library/types.html#types.FunctionType
+    # NOTE: VER: módulo types, porque usa types.MethodType to a bound instance method object,
+    # NOTE: y es una manera de pasar self: "Define names for built-in types that aren't directly accessible as a builtin."
     def __getattr__(self, name):
         try:
-            return self[name]
-        except KeyError as e:
-            msg = f"'{type(self).__name__}' has not known key '{name}'"
-            raise AttributeError(msg) from e # NOTE: creo que está bien que tire ambas excepciones, por posibles capturas puede ser uno u otro.
+            attr = self[name]
+        except KeyError:
+            if self.proto is not None:
+                try:
+                    attr = self.proto[name]
+                except KeyError:
+                    if self.parent is not None:
+                        try:
+                            attr = self.parent[name]
+                        except KeyError:
+                            attr = KeyError
+        if attr is KeyError:
+            msg = "'{}' has not attribute or key '{}'"
+            raise AttributeError(msg.format(type(self).__name__, name))
+        else:
+            if isinstance(attr, types.FunctionType):
+                # BUG: el problema es que por más que llame a Event.use, las variables con tilde siempre leen del entorno actual y puede cambiar
+                return types.MethodType(attr, self) # NOTE: esta es la forma correcta para que pase self primero siempre (no se puede fácil con FunctionType), ahora, self siempre tiene que estar declarada también. Cuando se llama con at no se pasa self en SuperCollider.
+            else:
+                return attr
+
 
     def __delattr__(self, name):
         if hasattr(type(self), name):
@@ -53,7 +77,43 @@ class Event(dict):
 
 
 class PitchEvent(Event):
-    pass
+    def _init(self):
+        self.mtranspose = 0
+        self.gtranspose = 0.0
+        self.ctranspose = 0.0
+        self.octave = 5.0
+        self.root = 0.0
+        self.degree = 0
+        self.scale = (0, 2, 4, 5, 7, 9, 11) # NOTE: ESTO SÍ ES UNA TUPLA EN VEZ DE UNA LISTA?
+        self.spo = 12.0 # NOTE steps per octave, steps_per_octave, stepsPerOctave. No sé.
+        self.detune = 0.0
+        self.harmonic = 1.0
+        self.octave_ratio = 2.0
+
+    # BUG: son property en realidad, al setear las llaves sobreescribe la función por un valor.
+    # BUG: ver si las pongo como llaves o como propiedades. El ejemplo clave es
+    # BUG: freq, que por defecto depende de (~midinote.value + ~ctranspose).midicps * ~harmonic;
+    # BUG: o midinote! y que se puedan llamar indistintamente como función o propiedad también es problema.
+    # BUG: cuando se especifica midinote. Pero en el otro extremo hay funciones
+    # BUG: como freqToNote y freqToScale, que actúan más como métodos.
+    def note(self):
+        # NOTE: usa degreeToKey, que es un método polimórfico y una UGen, por algo le dieron relevancia, pero ver la clase Scale porque me resulta redundante.
+        pass
+
+    def midinote(self):
+        pass
+
+    def detuned_freq(self):
+        pass
+
+    def freq(self):
+        pass
+
+    def freq2note(self, freq): # BUG: nombre. midicps, ampdb, freqnote/freqscale? freq_to_note?
+        pass
+
+    def freq2scale(self, freq):
+        pass
 
 
 class DurEvent(Event):

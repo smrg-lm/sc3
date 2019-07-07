acc643a6cb3fed39347bb15d2fe1ddb9460123b8
Lucas Samaruga <samarugalucas@gmail.com>
2019-07-06 21:54:23 -0300

    infougens.py

---
+++ sc3/infougens.py
@@ -0,0 +1,52 @@
+"""InfoUGens.sc"""
+
+from . import ugens as ugn
+
+
+class InfoUGenBase(ugn.UGen):
+    @classmethod
+    def ir(cls):
+        return cls.multi_new('scalar')
+
+
+class SampleRate(InfoUGenBase): pass
+class SampleDur(InfoUGenBase): pass
+class RadiansPerSample(InfoUGenBase): pass
+class BlockSize(InfoUGenBase): pass
+class ControlRate(InfoUGenBase): pass
+class ControlDur(InfoUGenBase): pass
+class SubsampleOffset(InfoUGenBase): pass
+
+
+class NumOutputBuses(InfoUGenBase): pass
+class NumInputBuses(InfoUGenBase): pass
+class NumAudioBuses(InfoUGenBase): pass
+class NumControlBuses(InfoUGenBase): pass
+class NumBuffers(InfoUGenBase): pass
+class NodeID(InfoUGenBase): pass
+
+
+class NumRunningSynths(InfoUGenBase):
+    @classmethod
+    def kr(cls):
+        return cls.multi_new('control')
+
+
+class BufInfoUGenBase(ugn.UGen):
+    @classmethod
+    def kr(cls, bufnum):
+        return cls.multi_new('control', bufnum)
+
+    @classmethod
+    def ir(cls, bufnum):
+        # // The .ir method is not the safest choice, since a buffer can be
+        # // reallocated at any time, using .ir will not track the changes.
+        return cls.multi_new('scalar', bufnum)
+
+
+class BufSampleRate(BufInfoUGenBase): pass
+class BufRateScale(BufInfoUGenBase): pass
+class BufFrames(BufInfoUGenBase): pass
+class BufSamples(BufInfoUGenBase): pass
+class BufDur(BufInfoUGenBase): pass
+class BufChannels(BufInfoUGenBase): pass

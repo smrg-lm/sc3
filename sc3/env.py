c8f552d0201bb8f1d733f10053867e796012e430
Lucas Samaruga <samarugalucas@gmail.com>
2019-07-22 04:01:12 -0300

    envgen.py completed.

--- sc3/env.py
+++ sc3/env.py
@@ -6,6 +6,16 @@ from . import graphparam as gpp
 class Env(gpp.UGenParameter, gpp.NodeParameter):
     # TODO: todo.
 
+    # BUG: si se necesita que sea general para otras clases hacer también __iter__?
+    # BUG: y que EnvGen siempre reciba un array de tuplas en envelope
+    # BUG: (como está hecho en sclang), pero no le veo sentido a simple vista.
+    def envgen_format(self):  # asMultichannelArray, se usa solo en Env y EnvGen.
+        if self._envgen_format is None:  # this.array
+            self._envgen_format = self._as_array()  # prAsArray
+        return self._envgen_format
+    def _as_array(self):
+        pass  # TODO: prAsArray, si hay variantes hacer dentro de envgen_format.
+
     ### UGen graph parameter interface ###
     # TODO: ver el resto en UGenParameter
 
--- sc3/ugens/envgen.py
+++ sc3/ugens/envgen.py
@@ -1,12 +1,114 @@
-# EnvGen.sc
+"""EnvGen.sc"""
 
 from .. import ugen as ugn
+from .. import env as env
 
-# TODO: todo...
+
+class Done(ugn.UGen):
+    NONE = 0
+    PAUSE_SELF = 1
+    FREE_SELF = 2
+    FREE_SELF_AND_PREV = 3
+    FREE_SELF_AND_NEXT = 4
+    FREE_SELF_AND_FREE_ALL_IN_PREV = 5
+    FREE_SELF_AND_FREE_ALL_IN_NEXT = 6
+    FREE_SELF_TO_HEAD = 7
+    FREE_SELF_TO_TAIL = 8
+    FREE_SELF_PAUSE_PREV = 9
+    FREE_SELF_PAUSE_NEXT = 10
+    FREE_SELF_AND_DEEP_FREE_PREV = 11
+    FREE_SELF_AND_DEEP_FREE_NEXT = 12
+    FREE_ALL_IN_GROUP = 13
+    FREE_GROUP = 14
+    FREE_SELF_RESUME_NEXT = 15
+
+    @classmethod
+    def kr(cls, src):
+        return cls._multi_new('control', src)
+
+
+class FreeSelf(ugn.UGen):
+    @classmethod
+    def kr(cls, input):
+        cls._multi_new('control', input)
+        return input
+
+
+class PauseSelf(ugn.UGen):
+    @classmethod
+    def kr(cls, input):
+        cls._multi_new('control', input)
+        return input
+
+
+class FreeSelfWhenDone(ugn.UGen):
+    @classmethod
+    def kr(cls, src):
+        return cls._multi_new('control', src)
+
+
+class PauseSelfWhenDone(ugn.UGen):
+    @classmethod
+    def kr(cls, src):
+        return cls._multi_new('control', src)
+
+
+class Pause(ugn.UGen):
+    @classmethod
+    def kr(cls, gate, id):
+        return cls._multi_new('control', gate, id)
+
+
+class Free(ugn.UGen):
+    @classmethod
+    def kr(cls, trig, id):
+        return cls._multi_new('control', trig, id)
+
+
+class EnvGen(ugn.UGen):
+    @classmethod
+    def ar(cls, envelope, gate=1.0, level_scale=1.0, level_bias=0.0,
+           time_scale=1.0, done_action=0):
+        '''
+        envelope can be a tuple, a list of tuples for multiple channels
+        or an instance of Env.
+        '''
+        if isinstance(envelope, env.Env):
+            envelope = envelope.envgen_format()
+        return cls._multi_new('audio', gate, level_scale, level_bias,
+                              time_scale, done_action, envelope)
+
+    @classmethod
+    def kr(cls, envelope, gate=1.0, level_scale=1.0, level_bias=0.0,
+           time_scale=1.0, done_action=0):
+        '''
+        envelope can be a tuple, a list of tuples for multiple channels
+        or an instance of Env.
+        '''
+        if isinstance(envelope, env.Env):
+            envelope = envelope.envgen_format()  # Was as_multichannel_list.
+        return cls._multi_new('control', gate, level_scale, level_bias,
+                              time_scale, done_action, envelope)
+
+    @classmethod
+    def _new1(cls, rate, *args):
+        obj = cls()
+        obj.rate = rate
+        obj.add_to_synth()
+        args = list(args)
+        envelope = args.pop()
+        return obj._init_ugen(*args, *envelope)
+
+    # Override may be an optimization in sclang.
+    # def _init_ugen(self, inputs)
+
+    def arg_names_inputs_offset(self):
+        return 2
+
 
 class Linen(ugn.UGen):
     @classmethod
     def kr(cls, gate=1.0, attack_time=0.01, sus_level=1.0,
            release_time=1.0, done_action=0):
-        return cls.multi_new('control', gate, attack_time, sus_level,
-                             release_time, done_action)
+        return cls._multi_new('control', gate, attack_time, sus_level,
+                              release_time, done_action)

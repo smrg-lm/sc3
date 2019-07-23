068fb69f2f5a336b3019f5b2180d330a1dca196a
Lucas Samaruga <samarugalucas@gmail.com>
2019-07-23 01:59:52 -0300

    Update setup.py, really.

--- setup.py
+++ setup.py
@@ -6,15 +6,14 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setuptools.setup(
-    name='sc3', # NOTE: no entiendo por qué el nombre de usuario para no pisar otro paquete, queda en el nombre?
-    version='0.0.1', # NOTE: https://www.python.org/dev/peps/pep-0440/ for tuning.
+    name='sc3',
+    version='0.0.1',
     author='Lucas Samaruga',
     author_email='samarugalucas@gmail.com',
     description='SuperCollider 3 class library Python 3 port',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url="https://github.com/smrg-lm/sc3",
-    #packages=setuptools.find_packages(exclude=['devtools', 'docs', 'looseparts', 'tests']), # NOTE: o poner ['sc3'], pero no se si luego voy a hacer subpaquetes. No es recomendable hacer subpaquetes por el anidamiento según dice el estilo.
     packages=['sc3'],
     python_requires='>=3.6',
     install_requires=['pyliblo'],

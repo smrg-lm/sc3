16c8f39e9641bfdb85ce986ad793805703393e72
Lucas Samaruga <samarugalucas@gmail.com>
2019-08-29 10:00:36 -0300

    Fix ugens __init__.
    Add synth.all convenience module.
    
    Revert _importall.py.

---
+++ sc3/synth/all.py
@@ -0,0 +1,10 @@
+
+from . buffer import *
+from . bus import *
+from . env import *
+from . node import *
+from . server import *
+from . synthdef import *
+from . synthdesc import *
+from . ugen import *
+from . ugens import *
--- sc3/synth/ugens/__init__.py
+++ sc3/synth/ugens/__init__.py
@@ -2,7 +2,21 @@
 Builtin UGen classes submodule.
 """
 
-from ...base import _importall
+import importlib as _importlib
+import pkgutil as _pkgutil
+import inspect as _inspect
+import sys as _sys
 
 
-installed_ugens =  _importall.import_all_classes(__path__, __name__)
+_module = _importlib.import_module('sc3.synth.ugen')
+_mapping = _inspect.getmembers(_module, _inspect.isclass)
+_sys.modules[__name__].__dict__.update(dict(_mapping))
+installed_ugens = dict(_mapping)
+
+for module_info in _pkgutil.walk_packages(__path__, __name__ + '.'):
+    if module_info.name.split('.')[-1][0] == '_':
+        continue
+    _module = _importlib.import_module(module_info.name)
+    _mapping = _inspect.getmembers(_module, _inspect.isclass)
+    _sys.modules[__name__].__dict__.update(dict(_mapping))
+    installed_ugens.update(dict(_mapping))

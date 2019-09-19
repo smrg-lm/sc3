4a3b562c42ee3654bfef8b27ca604c7ab685ccb1
Lucas Samaruga <samarugalucas@gmail.com>
2019-09-18 22:32:13 -0300

    Configure logging to use QueueHandler.

--- sc3/__init__.py
+++ sc3/__init__.py
@@ -1,8 +1,30 @@
 
-import logging as _logging
+import queue
+import logging
+import logging.handlers
+import atexit
 
-# This seems to be needed before any module logger is used.
-# TODO: log level in user config file.
-_logging.basicConfig(level=_logging.INFO)  # (format='%(levelname)s:%(name)s:%(message)s')
+
+### Configure logger ###
+
+# https://docs.python.org/3/howto/logging-cookbook.html#dealing-with-handlers-that-block
+
+q = queue.Queue(-1)
+queue_handler = logging.handlers.QueueHandler(q)
+listener_handler = logging.StreamHandler()
+listener = logging.handlers.QueueListener(q, listener_handler)
+
+formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+listener_handler.setFormatter(formatter)
+
+root_logger = logging.getLogger()
+root_logger.setLevel('INFO')
+root_logger.addHandler(queue_handler)
+
+listener.start()
+atexit.register(listener.stop)
+
+
+### Init library ###
 
 import sc3.base.main as _

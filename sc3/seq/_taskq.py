4bb5339256d9cd612428bb235959e5418d5f548d
Lucas Samaruga <samarugalucas@gmail.com>
2020-02-24 13:22:24 -0300

    TaskQueue added exception handling to remove method. Argument 'time' renamed to 'prio' as more general.

--- sc3/seq/_taskq.py
+++ sc3/seq/_taskq.py
@@ -24,48 +24,55 @@ class TaskQueue():
         self._counter = itertools.count()
         self._removed_counter = 0
 
-    def add(self, time, task):
-        'Add a new task or update the time of an existing task.'
+    def add(self, prio, task):
+        '''Add a new task or update the prio of an existing task.'''
         if task in self._entry_finder:
             self.remove(task)
         count = next(self._counter)
-        entry = [time, count, task]
+        entry = [prio, count, task]
         self._entry_finder[task] = entry
         heapq.heappush(self._queue, entry)
 
     def remove(self, task):
-        'Remove an existing task. Raise KeyError if not found.'
-        entry = self._entry_finder.pop(task)
-        entry[-1] = type(self)._REMOVED
-        self._removed_counter += 1
+        '''Remove an existing task. Does nothing if not found.'''
+        try:
+            entry = self._entry_finder.pop(task)
+            entry[-1] = type(self)._REMOVED
+            self._removed_counter += 1
+        except KeyError:
+            return
 
     def pop(self):
-        '''Remove and return the lowest time entry as a tuple (time, task).
-        Raise KeyError if empty.'''
+        '''
+        Remove and return the lowest prio entry as a tuple (prio, task).
+        Raise KeyError if empty.
+        '''
         while self._queue:
-            time, count, task = heapq.heappop(self._queue)
+            prio, count, task = heapq.heappop(self._queue)
             if task is not type(self)._REMOVED:
                 del self._entry_finder[task]
-                return (time, task)
+                return (prio, task)
             else:
                 self._removed_counter -= 1
         raise KeyError('pop from an empty task queue')
 
     def peek(self):
-        '''Return the lowest time entry as a tuple (time, task) without
-        removing it.'''
+        '''
+        Return the lowest prio entry as a tuple (prio, task) without
+        removing it.
+        '''
         for i in range(len(self._queue)):  # Can have <removed-task>s first.
-            time, count, task = self._queue[i]
+            prio, count, task = self._queue[i]
             if task is not type(self)._REMOVED:
-                return (time, task)
+                return (prio, task)
         raise KeyError('peek from an empty task queue')
 
     def empty(self):
-        'Return True if queue is empty.'
+        '''Return True if queue is empty.'''
         return (len(self._queue) - self._removed_counter) == 0
 
     def clear(self):
-        'Reset the queue to initial state (remove all tasks).'
+        '''Reset the queue to initial state (remove all tasks).'''
         self._init()
 
     # NOTE: implementar __iter__ y copy()

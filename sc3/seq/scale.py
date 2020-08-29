7f7832e448653373f9e2e819ea3db7acdb14a6c3
Lucas Samaruga <samarugalucas@gmail.com>
2020-08-29 19:21:52 -0300

    Degree can be a Rest from patterns.

--- sc3/seq/scale.py
+++ sc3/seq/scale.py
@@ -27,7 +27,7 @@ class Scale(list): # BUG: Tuning es como un array en sc y Scale implementa la in
         if spo is None:
             spo = self.tuning.spo()
         l = len(self)
-        base_key = spo * (degree // l) + self[degree % l]
+        base_key = spo * (degree // l) + self[int(degree) % l]
         if acc == 0:
             return base_key
         else:

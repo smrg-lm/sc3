e5f6542a9024961ae3f7e5d5a5b6f4fdcce206af
Lucas Samaruga <samarugalucas@gmail.com>
2020-10-12 19:15:45 -0300

    Added incomplete test files.

---
+++ tests/test_bundle.py
@@ -0,0 +1,35 @@
+
+import unittest
+
+import sc3
+from sc3.base.netaddr import NetAddr
+from sc3.base.responsedefs import OscFunc
+
+sc3.init()
+
+
+class BundleTestCase(unittest.TestCase):
+    def test_nested_bundle_time(self):
+        # ValueError('nested bundle time must be >= enclosing bundle time').
+        n = NetAddr('127.0.0.1', NetAddr.lang_port())
+        self.assertRaises(
+            ValueError,
+            lambda n: n.send_bundle(1, ['/msg', 1], [0.5, ['/msg', 2]]), n);
+        self.assertRaises(
+            ValueError,
+            lambda n: n.send_bundle(0, ['/msg', 1], [None, ['/msg', 2]]), n);
+
+    # BUG: Messages should have exactly the same decimal part (as in sclang).
+    # This is needed for nested bundle times. Requires low level change
+    # to UDP/TCP code. Is not critical.
+    # def test_processing_time(self):
+    #     n = NetAddr('127.0.0.1', 57120)
+    #     o = OscFunc(lambda *args: print(args), '/msg')
+    #     # Same time of arrival.
+    #     n.send_bundle(0, ['/msg', 1], ['/msg', 2])
+    #     n.send_bundle(0, ['/msg', 1], [1, ['/msg', 2]])
+    #     n.send_bundle(0, [1, ['/msg', 1], [2, ['/msg', 2]]])
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_synthdef.py
@@ -0,0 +1,79 @@
+
+import unittest
+
+import sc3
+from sc3.synth.synthdef import SynthDef, synthdef
+from sc3.synth.ugens.line import DC
+from sc3.synth.ugens.inout import Out
+
+sc3.init()
+
+
+class SynthDefTestCase(unittest.TestCase):
+    def test_dummy(self):
+        ...
+
+    def test_build(self):
+        def graph():
+            Out.ar(0, DC.ar(0.5) * 1)
+
+        sd = SynthDef('test', graph)
+        self.assertEqual(sd.name, 'test')
+        self.assertIs(sd.func, graph)
+        self.assertEqual(sd.metadata, dict())
+        self.assertEqual(sd.variants, dict())
+        self.assertIsNone(sd.desc)
+
+        # Content, maybe goes in a test_synthdesc.py file
+        # and/or test_synthdef_optimizations.py.
+        self.assertEqual(sd._constants, {0.5: 0, 0.0: 1})
+        ugen_set = {DC, Out}
+        for u in sd._children:
+            self.assertTrue(type(u) in ugen_set)
+
+    def test_default_rate(self):
+        ...
+
+    def test_array_controls(self):
+        ...
+
+    def test_controls(self):  # test_rates
+        ...
+
+    def test_prepend_args(self):
+        ...
+
+    def test_variants(self):
+        ...
+
+    def test_metadata(self):
+        ...
+
+    def test_add(self):
+        ...
+
+    def test_send(self):
+        ...
+
+    def test_store(self):
+        ...
+
+    def test_load(self):
+        ...
+
+    def test_wrap(self):
+        ...
+
+    def test_as_bytes(self):
+        ...
+
+    # test or remove store_once.
+    # remove store_at.
+    # write_def and write_def_list should be internal.
+
+    def test_decorator(self):
+        ...
+
+
+if __name__ == '__main__':
+    unittest.main()

6c1ae87f7970e0e26cd8b5a531b7de021fe25a18
Lucas Samaruga <samarugalucas@gmail.com>
2020-11-19 01:16:19 -0300

    Add play function (experimental)
    In this case is also a shortcut to play events from dictionaries
    or keyword arguments which seems to be more important because we
    don't have a syntax shortcut. The problem is that there is no way
    to play a function with server arguments because of that. This
    may change in the future, somehow.

--- sc3/base/all.py
+++ sc3/base/all.py
@@ -7,6 +7,7 @@ from . model import *
 from . netaddr import *
 from . operand import *
 from . platform import *
+from . play import *
 from . responsedefs import *
 from . stream import *
 from . systemactions import *
---
+++ sc3/base/play.py
@@ -0,0 +1,104 @@
+"""Global play shortcut mostly for events and lambdas."""
+
+import logging
+import inspect
+
+from . import utils as utl
+from . import responsedefs as rdf
+from ..seq import event as evt
+from ..synth import synthdef as sdf
+from ..synth import _graphparam as gpp
+from ..synth import ugen as ugn
+from ..synth import ugens as ugns
+from ..synth import env
+from ..synth import systemsynthdefs as ssd
+from ..synth import node as nod
+
+
+__all__ = ['play']
+
+
+_logger = logging.getLogger(__name__)
+
+
+def _play_func(func, target=None, outbus=0, fade=0.01,
+               add_action='addToHead', args=()):
+    # asDefName.sc
+    target = gpp.node_param(target)._as_target()
+    server = target.server
+    if not server._status_watcher.server_running:
+        _logger.warning(f'server {str(server.name)} not running')
+        return
+
+    # GraphBuilder.sc
+    def wrapper(_iout:'ir'=0):
+        # SynthDef.wrap function has to return an UGen-input or
+        # None to be chained or not. UGens that don't return a
+        # signal should return None. Generated synthdef of this
+        # version of func.play() differs a bit from the original.
+        result = sdf.SynthDef.wrap(func)
+        if result is not None:
+            if fade:
+                ugns.Control.add_name('gate')
+                gate = ugns.Control.kr(1.0)
+                result *= ugns.EnvGen.kr(
+                    env.Env.asr(fade, 1, fade, 'lin'), gate, 1, 0, 1, 2)
+            result = ugn.SynthObject._replace_zeroes_with_silence(
+                utl.as_list(result))
+            rate = gpp.ugen_param(result)._as_ugen_rate()
+            ugns.Out._multi_new(rate, _iout, *result)
+
+    synthdef = sdf.SynthDef(ssd.generate_tmp_name(), wrapper)
+    synth = nod.Synth.basic_new(synthdef.name, server)
+    rdf.OscFunc(
+        # // Use the /n_end signal to remove the temp synthdef.
+        lambda *_: server.send_msg('/d_free', synthdef.name),
+        '/n_end', server.addr, arg_template=[synth.node_id]).one_shot()
+    args = gpp.node_param(args)._as_control_input()
+    synth_msg = synth.new_msg(
+        target, ['_iout', outbus, 'out', outbus, *args], add_action)
+    synthdef._do_send(server, synth_msg)
+    print('*** synthdef.dump_ugens:'); synthdef.dump_ugens()
+    return synth
+
+
+def play(obj=None, *args, **kwargs):
+    '''Convenience function to play events and lambdas.
+
+    If called without any argument it will play the default event
+    with the default parameters.
+
+    If called only with keyword arguments create an event with
+    those keys and plays it.
+
+    If ``obj`` defines the method ``play`` just calls it with
+    *args and **kwargs.
+
+    If ``obj`` is a dict, it creates an event and plays it adding
+    kwargs if any. Only keyword arguments are allowed for this case.
+
+    If ``obj`` is a function, it creates a temp SynthDef and makes a
+    Synth from it. The function should return the expression to be
+    played or have it's own output. This is mostly intended for simple
+    lambda functions but can be used as a decorator.
+
+    All versions return the return value of the played object.
+    '''
+
+    if not obj and not args and not kwargs:
+        # Test tone.
+        return evt.event().play()  # TODO: Event is not returning the created server object.
+    elif hasattr(obj, 'play'):
+        # Is playable.
+        return obj.play(*args, **kwargs)
+    elif isinstance(obj, dict) and not args:
+        # As event.
+        return evt.event({**obj, **kwargs}).play()
+    elif inspect.isfunction(obj):
+        # Decorator or play(lmbd, s, 0, ...).
+        return _play_func(obj, *args, **kwargs)
+    elif obj is None and not args and kwargs:
+        # As a keyword only call makes an event.
+        return evt.event(kwargs).play()
+    else:
+        raise ValueError(f'non playable arguments')

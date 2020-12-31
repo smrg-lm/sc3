27d892adebe52690d6cb4b478117f8523c898ecd
Lucas Samaruga <samarugalucas@gmail.com>
2020-12-31 19:20:29 -0300

    Fix old Bus test

--- tests/test_bus.py
+++ tests/test_bus.py
@@ -14,7 +14,6 @@ class BusTestCase(unittest.TestCase):
     def test_common_interface(self):
         b1 = ControlBus(5)
         b2 = ControlBus.new_from(b1, 3)
-        self.assertEqual(b1.rate, b2.rate)
         self.assertEqual(b1.index + 3, b2.index)
         self.assertEqual(b2.channels, 1)
         ...
@@ -22,7 +21,6 @@ class BusTestCase(unittest.TestCase):
     def test_audiobus_interface(self):
         b = AudioBus()
         self.assertEqual(b.channels, 1)
-        self.assertEqual(b.rate, 'audio')
         b = AudioBus(8)
         self.assertEqual(b.channels, 8)
         ...
@@ -30,7 +28,6 @@ class BusTestCase(unittest.TestCase):
     def test_controlbus_interface(self):
         b = ControlBus()
         self.assertEqual(b.channels, 1)
-        self.assertEqual(b.rate, 'control')
         b = ControlBus(8)
         self.assertEqual(b.channels, 8)
         ...
